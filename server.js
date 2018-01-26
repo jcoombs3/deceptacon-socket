2253a50c182eb451fbf4afcd7b113218888669f5
John Coombs <johnc@backbase.com>
2018-01-26 00:30:19 -0500

    reset server

--- server.js
+++ server.js
@@ -1,52 +1,19 @@
 'use strict';
 
-var path = require('path');
-var bodyParser = require('body-parser');
-var cors = require('cors');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-const PORT = process.env.PORT || 4200;
-
-// use body parser so we can get info from POST and/or URL parameters
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(cors({ origin: '*' }));
-// Settings for CORS
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.header('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
-
-    // Pass to next layer of middleware
-    next();
+app.use(bodyParser.json());
+app.set('port', process.env.PORT || 8080);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
-var server = app.listen(PORT);
-var io = require('socket.io').listen(server, {
-    log: false,
-    agent: false,
-    origins: '*:*',
-    transports: ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling', 'polling']
-});
+app.use(express.static("www")); 
 
-io.on('connection', (socket) => {
-  console.log('++ a client connected...');
-  
-  socket.on('com.deceptacon.event', function (arr) {
-    console.log('++ com.deceptacon.event', arr.event);
-    io.sockets.emit(arr.event, arr.data);
-  });
-  
-  socket.on('disconnect', () => console.log('++ a client disconnected...'));
+app.listen(app.get('port'), function () {
+  console.log("You're a wizard, Harry. I'm a what? Yes, a wizard, on port", app.get('port'));
 });
