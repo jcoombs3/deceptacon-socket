9d0d3afc76fb38b754b1707b298c1c4d67443a90
John Coombs <johnc@backbase.com>
2018-01-26 00:33:58 -0500

    origins

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static("www"));
 //});
 
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var io = require('socket.io')(server, {origins: '*:*'});
 
 server.listen(app.get('port'), function(){
   console.log('listening on *:4200');
