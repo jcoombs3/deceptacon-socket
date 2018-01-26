ae5f146452d32e91d6aebc8669193aeb885e1494
John Coombs <johnc@backbase.com>
2018-01-26 00:14:39 -0500

    test

--- server.js
+++ server.js
@@ -1,29 +1,57 @@
 'use strict';
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
-const PORT = 4200;
-const INDEX = path.join(__dirname, 'index.html');
-
-const server = express()
-  .use((req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.sendFile(INDEX)
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const io = socketIO(server);
-
-io.on('connection', (socket) => {
-  console.log('++ a client connected...');
-  
-  socket.on('com.deceptacon.event', function (arr) {
-    console.log('++ com.deceptacon.event', arr.event);
-    io.sockets.emit(arr.event, arr.data);
-  });
-  
-  socket.on('disconnect', () => console.log('++ a client disconnected...'));
+app.use(bodyParser.json());
+app.set('port', 4200);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
+app.use(express.static("www"));
+
+app.listen(app.get('port'), function () {
+  console.log("You're a wizard, Harry. I'm a what? Yes, a wizard, on port", app.get('port'));
+});
+
+
+
+
+
+
+
+
+
+//
+//
+//const express = require('express');
+//const socketIO = require('socket.io');
+//const path = require('path');
+//
+//const PORT = 4200;
+//const INDEX = path.join(__dirname, 'index.html');
+//
+//const server = express()
+//  .use((req, res) => {
+//    res.header("Access-Control-Allow-Origin", "*");
+//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//    res.sendFile(INDEX)
+//  })
+//  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+//
+//const io = socketIO(server);
+//
+//io.on('connection', (socket) => {
+//  console.log('++ a client connected...');
+//  
+//  socket.on('com.deceptacon.event', function (arr) {
+//    console.log('++ com.deceptacon.event', arr.event);
+//    io.sockets.emit(arr.event, arr.data);
+//  });
+//  
+//  socket.on('disconnect', () => console.log('++ a client disconnected...'));
+//});
