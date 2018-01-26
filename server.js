868cd064f4f9dee42dbe284be1ec752e2c299ad2
John Coombs <johnc@backbase.com>
2018-01-26 00:52:09 -0500

    new socket

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-'use strict';
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+const PORT = process.env.PORT || 3231
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+app.use(express.static("www"))
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const io = socketIO(server);
-
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+io.on('connection', function(socket) {  
+    console.log('++ a client connected...');
+  
+    socket.on('com.deceptacon.event', function (arr) {
+      console.log('++ com.deceptacon.event', arr.event);
+      io.sockets.emit(arr.event, arr.data);
+    });
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+app.listen(PORT, ()=>{
+  console.log("Connected to port:" + PORT);
+})
