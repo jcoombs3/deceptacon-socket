8c334d61a0a3b9f7c559337d634f6554fca55461
John Coombs <johnc@backbase.com>
2018-01-26 01:03:02 -0500

    socket test

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
-express = require('express');  
-var app = express();  
-var server = require('http').createServer(app);  
-var io = require('socket.io')(server);
-
-app.use(express.static("www")); 
-// Our Ionic app build is in the www folder 
-// (kept up-to-date by the Ionic CLI using 'ionic serve')
-
-server.listen(4200, function(){
-  console.log('listening on *:4200');
-});
-
-io.on('connection', function(socket) {  
-    console.log('++ a client connected...');
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+ 
+io.on('connection', (socket) => {
+  console.log('++ a client connected...');
   
-    socket.on('com.deceptacon.event', function (arr) {
-      console.log('++ com.deceptacon.event', arr.event);
-      io.sockets.emit(arr.event, arr.data);
-    });
+  socket.on('com.deceptacon.event', function (arr) {
+    console.log('++ com.deceptacon.event', arr.event);
+    io.sockets.emit(arr.event, arr.data);
+  });
+});
+ 
+var port = process.env.PORT || 3001;
+ 
+http.listen(port, function(){
+   console.log('listening in http://localhost:' + port);
 });
