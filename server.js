442e9254ee1ae3399843c3c689835ea28f7b12b2
John Coombs <johnc@backbase.com>
2018-01-26 00:32:44 -0500

    more funcs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var io = require('socket.io')(server);
 
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 8080);
@@ -14,6 +15,22 @@ app.use(function(req, res, next) {
 
 app.use(express.static("www")); 
 
-app.listen(app.get('port'), function () {
-  console.log("You're a wizard, Harry. I'm a what? Yes, a wizard, on port", app.get('port'));
+//app.listen(app.get('port'), function () {
+//  console.log("You're a wizard, Harry. I'm a what? Yes, a wizard, on port", app.get('port'));
+//});
+
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+server.listen(app.get('port'), function(){
+  console.log('listening on *:4200');
+});
+
+io.on('connection', function(socket) {  
+  console.log('++ a client connected...');
+
+  socket.on('com.deceptacon.event', function (arr) {
+    console.log('++ com.deceptacon.event', arr.event);
+    io.sockets.emit(arr.event, arr.data);
+  });
 });
