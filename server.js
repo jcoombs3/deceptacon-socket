00b92a6d0bd8461ae1e04c58c32a5790a95af9a5
John Coombs <johnc@backbase.com>
2018-01-25 23:54:16 -0500

    socket updates for heroku

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-express = require('express');  
-var app = express();  
-var server = require('http').createServer(app);  
-var io = require('socket.io')(server);
+const io = socketIO(server);
 
-app.use(express.static("www")); 
-// Our Ionic app build is in the www folder 
-// (kept up-to-date by the Ionic CLI using 'ionic serve')
-
-server.listen(4200, function(){
-  console.log('listening on *:4200');
-});
-
-io.on('connection', function(socket) {  
-    console.log('++ a client connected...');
+io.on('connection', (socket) => {
+  console.log('++ a client connected...');
   
-    socket.on('com.deceptacon.event', function (arr) {
-      console.log('++ com.deceptacon.event', arr.event);
-      io.sockets.emit(arr.event, arr.data);
-    });
+  socket.on('com.deceptacon.event', function (arr) {
+    console.log('++ com.deceptacon.event', arr.event);
+    io.sockets.emit(arr.event, arr.data);
+  });
+  
+  socket.on('disconnect', () => console.log('++ a client disconnected'));
 });
